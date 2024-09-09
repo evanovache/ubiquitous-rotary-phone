038ad4afa166e8b5819274bb1bc8b93c5a52122b
Evans Keborni <evanovache78@gmail.com>
2024-09-09 15:20:53 +0000

    Solve issue with saving the form

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,14 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
